b631281f35eb795d7178747682943ed117e04e27
Emeka Augustine <cafafanscoders@gamil.com>
2019-02-21 16:48:24 -0500

    Fixed minor bugs

--- app/Http/Controllers/Api/Admin/AdminController.php
+++ app/Http/Controllers/Api/Admin/AdminController.php
@@ -60,7 +60,7 @@ class AdminController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Admin $admin)
     {
         return new AdminResource($admin);
     }
--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -60,7 +60,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
         return new UserResource($user);
     }
